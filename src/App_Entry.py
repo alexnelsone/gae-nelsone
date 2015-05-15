8025b1280133a20f10ac105e958c568b222f8d27
alexnelsone <nelsone@nelsone.com>
2015-05-14 21:49:48 -0700

    playing around with login options

--- src/App_Entry.py
+++ src/App_Entry.py
@@ -1,9 +1,11 @@
 import webapp2
 import MainApp
+import User_Login
 
 
 app = webapp2.WSGIApplication([
     #I would eventually like to post the same URL
     ('/', MainApp.MainApp),
+    ('/protected', User_Login.UserLogin),
     
 ], debug=True)
---
+++ src/User_Login.py
@@ -0,0 +1,38 @@
+import os
+import urllib
+import webapp2
+import jinja2
+from google.appengine.api import users
+
+TEMPLATE_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+template = TEMPLATE_ENVIRONMENT.get_template('./site_files/login_test.html')
+
+
+class UserLogin(webapp2.RequestHandler):
+    def get(self):
+        
+        user = users.get_current_user()
+        login_url = users.create_login_url(self.request.uri)
+        logout_url = users.create_logout_url(self.request.uri)
+        
+        #reverse this. It would be easier.  So if user NOT logged in redirect to login page.
+        #if logged in do a bunch of other stuff.
+        
+        if user:
+            context = {
+                    'page': "home",
+                    'current_url' : self.request.url,
+                     'login_url': login_url,
+                    'logout_url': logout_url,
+                    'user': user,
+                    'response': ''
+                  }
+        
+            self.response.out.write(template.render(context))
+        else:
+            
+            self.redirect(users.create_login_url(self.request.uri))
